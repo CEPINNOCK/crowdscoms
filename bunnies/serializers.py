72c3bdc6acff207789d501927d99ae735a3d3996
CEPINNOCK <edwardfinancee@outlook.com>
2023-02-27 02:46:01 +0000

    new

--- bunnies/serializers.py
+++ bunnies/serializers.py
@@ -1,15 +1,21 @@
-from rest_framework import serializers
+#Import the necessary modules
 
+from rest_framework import serializers
 from bunnies.models import Bunny, RabbitHole
 
 
 class RabbitHoleSerializer(serializers.ModelSerializer):
+    """Serializer for RabbitHole model"""
 
+    # Specify related field for bunnies and queryset for PrimaryKeyRelatedField
     bunnies = serializers.PrimaryKeyRelatedField(many=True, queryset=Bunny.objects.all())
+
+    # Create custom serializer method to get bunny count for each rabbit hole
     bunny_count = serializers.SerializerMethodField()
 
     def get_bunny_count(self, obj):
-        return Bunny.objects.count()
+        """Method to get bunny count for each rabbit hole"""
+        return obj.bunnies.count()
 
     class Meta:
         model = RabbitHole
@@ -17,17 +23,22 @@ class RabbitHoleSerializer(serializers.ModelSerializer):
 
 
 class BunnySerializer(serializers.ModelSerializer):
+    """Serializer for Bunny model"""
 
+    # Specify related field for home and queryset for SlugRelatedField
     home = serializers.SlugRelatedField(queryset=RabbitHole.objects.all(), slug_field='location')
+
+    # Create custom serializer method to get family members for each bunny
     family_members = serializers.SerializerMethodField()
 
     def get_family_members(self, obj):
+        """Method to get family members for each bunny"""
         return []
 
     def validate(self, attrs):
+        """Method to validate data"""
         return attrs
 
     class Meta:
         model = Bunny
         fields = ('name', 'home', 'family_members')
-
--- bunnies/views.py
+++ bunnies/views.py
@@ -1,6 +1,5 @@
+# Import the necessary modules
 from rest_framework import viewsets
-
-# Create your views here.
 from rest_framework.permissions import IsAuthenticated
 
 from bunnies.models import Bunny, RabbitHole
@@ -9,18 +8,30 @@ from bunnies.serializers import BunnySerializer, RabbitHoleSerializer
 
 
 class RabbitHoleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing rabbit holes instances.
+    """
     serializer_class = RabbitHoleSerializer
     permission_classes = (IsAuthenticated, RabbitHolePermissions)
     queryset = RabbitHole.objects.all()
 
     def create(self, request, *args, **kwargs):
+        """
+        Override the create method to add custom logic.
+        """
         return super().create(request, *args, **kwargs)
 
     def filter_queryset(self, queryset):
+        """
+        Override the filter_queryset method to add custom filtering logic.
+        """
         return queryset
 
 
 class BunnyViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing bunny instances.
+    """
     serializer_class = BunnySerializer
     permission_classes = (IsAuthenticated,)
     queryset = Bunny.objects.all()
