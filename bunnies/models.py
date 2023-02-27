2a9179bb93fe9592efbd9c759639ea3230ce3f86
CEPINNOCK <edwardfinancee@outlook.com>
2023-02-27 02:50:40 +0000

    add-comments to models

--- bunnies/models.py
+++ bunnies/models.py
@@ -1,21 +1,38 @@
+# Import the necessary modules
+
 from django.conf import settings
 from django.db import models
 
-# Create your models here.
-
 
 class RabbitHole(models.Model):
-    '''
-    Rabbits live in rabbit holes
-    '''
+    """
+    Represents a rabbit hole where bunnies live.
+    """
+
+    # Location of the rabbit hole
     location = models.CharField(max_length=64, unique=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # Owner of the rabbit hole
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    # Maximum number of bunnies allowed in the rabbit hole
     bunnies_limit = models.PositiveIntegerField(default=5)
 
 
 class Bunny(models.Model):
-    '''
+    """
+    Represents a bunny that lives in a rabbit hole.
+    """
 
-    '''
+    # Name of the bunny
     name = models.CharField(max_length=64)
-    home = models.ForeignKey(RabbitHole, on_delete=models.CASCADE, related_name='bunnies')
+
+    # Rabbit hole where the bunny lives
+    home = models.ForeignKey(
+        RabbitHole,
+        on_delete=models.CASCADE,
+        related_name='bunnies'
+    )
